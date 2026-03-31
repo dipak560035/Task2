ab2b24dbe5efa20e022656bd60e13705ad0da55d
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-03-31 08:14:57 +0545

    Fix comment formatting in hero.js

--- hero.js
+++ hero.js
@@ -1,4 +1,4 @@
 import { heroui } from "@heroui/react";
-// or import from theme package if you are using individual packag
+// or import from theme package if you are using individual pack
 
 export default heroui();
