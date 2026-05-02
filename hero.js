9b872facf4211228c9b34d19cec93920063cc3f5
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-02 09:55:37 +0545

    Remove duplicate import and export statements

--- hero.js
+++ hero.js
@@ -5,5 +5,10 @@ import { heroui } from "@heroui/react";
 export default heroui();
 import { heroui } from "@heroui/react";
 export default heroui();
+import { heroui } from "@heroui/react";
+export default heroui();
+
+
+
 
 
