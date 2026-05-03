c16f14b20e9632a1a252eed64edae2e8850c2eab
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-03 10:21:29 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,17 @@ import App from './App.jsx'
 import { HeroUIProvider } from '@heroui/react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+iimport { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
+import { HeroUIProvider } from '@heroui/react'
+
+createRoot(document.getElementById('root')).render(
+  <HeroUIProvider>
+    <App />
+  </HeroUIProvider>
+)
+mport App from './App.jsx'
 import { HeroUIProvider } from '@heroui/react'
 
 createRoot(document.getElementById('root')).render(
