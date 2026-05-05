ca76671699d0f8da1587262eb050caae17fb9800
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-05 21:44:39 +0545

    Update hero.js

--- hero.js
+++ hero.js
@@ -4,12 +4,3 @@ export default heroui();
 
 
 
-
-
-import { heroui } fimport { heroui } from "@heroui/react";
-export default heroui();rom "@heroui/react";
-export defauimport { himport { heroui } from "@heroui/react";
-export default heroui();eroui } from "@heroui/react";
-export default heroui();lt heroui();
-import { heroui } from "@heroui/react";
-export default heroui();
